b4579e9c2159be740e6eb78371aa685ed8b6c9c9
NakOh <kk0704@naver.com>
2015-05-26 22:58:18 +0900

    새로운 NetworkGame 따라가기 472Page까지 완료

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCtrl : MonoBehaviour {
+	private Transform tr;
+	private NetworkView _networkView;
+
+	void Awake(){
+		tr = GetComponent<Transform> ();
+		_networkView = GetComponent<NetworkView> ();
+
+		if (_networkView.isMine) {
+			Camera.main.GetComponent<SmoothFollow>().target = tr;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
